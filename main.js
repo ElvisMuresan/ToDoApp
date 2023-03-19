9c2bcd1b18b98ef29e50cfe3be17ac5a5d3c9a46
ElvisMuresan <elvis.e.muresan@gmail.com>
2023-03-19 15:58:57 +0200

    Changed the actions buttons into icons

--- main.js
+++ main.js
@@ -1,180 +1,124 @@
+let bodyElement = document.body;
+let inputElement = document.getElementById("inputId");
+let addTodoElement = document.getElementById("buttonId");
+let listaParinteElement = document.getElementById("listaParinteId");
+let notificationElement = document.getElementById("notification");
 
-let inputElement = document.getElementById('inputId');
-let buttonElement = document.getElementById('buttonId');
-let popup = document.getElementById('popup');
-//Practic e un div in care avem lista de to do-uri
-let listaParinteElement = document.getElementById('listaParinteId');
+/**
+ * Show notification
+ */
 
-// const success = document.getElementById('success');
-// const filledIn = document.getElementById('filledIn');
+function showNotification(inputValue) {
+  let notificationConfig = {
+    style: "display: block; color: blue",
+    text: "To do succesfully added!",
+  };
 
-
-// function openPopup() {
-//   popup.classList.add("open-popup");
-// }
-// function closePopup() {
-//   popup.classList.remove("open-popup");
-// }
-
-//Event Listeners.  Crearea evenimentului cand apasam buttonElement si functioa addToDo
-buttonElement.addEventListener('click', addToDo);
-
-//Functions
-//Function Add task
-function addToDo(event) {
-  event.preventDefault();
-  // Declararea constantei input care ia valoarea lui inputElement
-  const input = inputElement.value;
-
-  //Crearea elementului div in care vom avea content-ul si actions
-  const task_div = document.createElement("div");
-
-  //Crearea clasei task in task_div
-  task_div.classList.add("task");
-
-  //Crearea elementului div in care este clasa content
-  const task_content_div = document.createElement("div");
-
-  //Crearea clasei content in care vom avea clasa text cu continutul inputului
-  task_content_div.classList.add("content");
-
-  //aceasta comanda ne adauga continutul lui task_content_div in task_div si afiseaza textul 
-  task_div.appendChild(task_content_div);
-
-  //Crearea elementului input care va fi asociat constantei task_input_div
-  const task_input_div = document.createElement("input");
-
-  //Crearea clasei text in care vom avea textul in task_input_div
-  task_input_div.classList.add("text");
-
-  //Type-ul sa fie text pentru constanta
-  task_input_div.type = "text";
-
-  //Valoarea lui task_input_div va fi in input. Practic ne scrie textul in toDo-ul afisat
-  task_input_div.value = input;
-
-  //Setam atributul de readnonly pentru task_input_div.Practic sa nu putem modica textul scris din input
-  task_input_div.setAttribute("readonly", "readonly");
-
-  //Constanta task_input_div este child-ul lui task_content_div
-  task_content_div.appendChild(task_input_div);
-
-  //Crearea div in care avem cele 3 butoane(cheched, edit si delete)
-  const task_actions_div = document.createElement("div");
-  task_actions_div.classList.add("actions");
-
-  //Crearea buttonului checked
-  const task_checked_div = document.createElement("button");
-  task_checked_div.classList.add("checked");
-  task_checked_div.innerHTML = "Checked"; //Afisarea butonului checked
-
-  const task_edit_div = document.createElement("button");
-  task_edit_div.classList.add("edit");
-  task_edit_div.innerHTML = "Edit";
-
-  const task_delete_div = document.createElement("button");
-  task_delete_div.classList.add("delete");
-  task_delete_div.innerHTML = "Delete";
-
-  task_actions_div.appendChild(task_checked_div);
-  task_actions_div.appendChild(task_edit_div);
-  task_actions_div.appendChild(task_delete_div);
-
-
-  task_div.appendChild(task_actions_div);
-
-  listaParinteElement.appendChild(task_div); // Adauga task-ul
-  inputElement.value = ""; // ne curata labelul dupa ce adaugam un toDo
-
-  const task_message_div = document.createElement("div");
-  task_message_div.classList.add("message");
-  const task_filledIn_div = document.createElement("p");
-  task_filledIn_div.classList.add("filledIn");
-  task_filledIn_div.setAttribute("id", "filledIn");
-  task_filledIn_div.innerText = "Please insert a task!";
-
-  const task_succes_div = document.createElement("p");
-  task_succes_div.classList.add("success");
-  task_succes_div.setAttribute("id", "success");
-  task_succes_div.innerText = "To do succesfully added!";
-
-  task_message_div.appendChild(task_filledIn_div);
-  task_message_div.appendChild(task_succes_div);
-
-  listaParinteElement.appendChild(task_message_div);
-
-  if (!input) {
-    //alert("Please fill out the task!");
-    //return;
-    task_filledIn_div.style.display = 'block';
-    listaParinteElement.removeChild(task_div);
-  }
-  else {
-    setTimeout(() => {
-      task_succes_div.style.display = 'block';
-    }, 500);
-
+  if (!inputValue) {
+    notificationConfig = {
+      style: "display: block; color: red",
+      text: "Please insert a task!",
+    };
   }
 
-  setTimeout(() => {
-    task_succes_div.style.display = 'none';
-    task_filledIn_div.style.display = 'none';
-  }, 4000);
-
-  task_checked_div.addEventListener('click', checkedTask);
-  //Checked function
-  /* Functia checked in care daca task_checked_div este egal cu
-  "checked" ne sublineaza textul folosind text decoration style
-  - line-through. Apoi butonul check se schimba in unchecked. 
-  Pentru ca butonul e pe unchecked o sa intre in else si il schimba
-  pe checked si apoi textDecoration style se muta pe none. Practic 
-  se uncheck-uie textul. */
-  function checkedTask(e) {
-    if (task_checked_div.innerText.toLowerCase() == "checked") {
-      task_content_div.style.textDecoration = "line-through";
-      task_checked_div.innerText = "Unchecked";
-    } else {
-      task_checked_div.innerText = "Checked";
-      task_content_div.style.textDecoration = "none";
-    }
-  }
-
-  // Aici adaugam un eveniment care contine functia de editare
-  task_edit_div.addEventListener('click', editTask);
-
-  /* E aceiasi metoda ca functia de checked. Ce am adaugat aici
-  este:
-  - am scos atributul de readonly pentru a putea modifica textul
-  - metoda de focus pe text dupa ce apasam butonul de edit */
-  function editTask(e) {
-    if (task_edit_div.innerText.toLowerCase() == "edit") {
-      task_edit_div.innerText = "Save";
-      task_input_div.removeAttribute("readonly");
-      task_input_div.focus();
-    } else {
-      task_edit_div.innerText = "Edit";
-      task_input_div.setAttribute("readonly", "readonly");
-    }
-  }
-
-
-  //Evenimentul in care apelam functia de stergere a unui toDo
-  task_delete_div.addEventListener('click', deleteTask);
-  //Delete function in care stergem child-ul task_div din ListaParinte
-  function deleteTask(e) {
-    listaParinteElement.removeChild(task_div);
-  }
-
-
-
-  /* Message task. Aici avem un if in care daca input nu are continut
- adaugam div-ul filled id care este pus manual in html si eliminam
- din lista Parinte task_div care reprezinta tot label-ul. Altfel
- set un timeout in care afisam div-ul succes in care avem mesajul
- de succes. Apoi dupa 4 secunde eliminam cele doua div-uri. Ele 
- exista dar nu le mai afisam pe ecran*/
-
+  notificationElement.style = notificationConfig.style;
+  notificationElement.innerText = notificationConfig.text;
+  setTimeout(() => (notificationElement.style.display = "none"), 4000);
 }
 
+/**
+ * Creates todo item
+ */
+function createTodo(inputValue) {
+  // add task element
+  let todoElement = document.createElement("div");
+  todoElement.classList.add("task");
+
+  // input
+  const todoInputElement = document.createElement("input");
+  todoInputElement.classList.add("text");
+  todoInputElement.type = "text"; 
+  todoInputElement.value = inputValue;
+  todoInputElement.setAttribute("readonly", "");
+
+  // create task content
+  const todoContentElement = document.createElement("div");
+  todoContentElement.classList.add("content");
+  todoContentElement.appendChild(todoInputElement);
+
+  // buttons
+  
+  // create the checkbox element 
+  const checkbox = document.createElement("input");
+
+  // set the type to checkbox
+  checkbox.type = 'checkbox';
+
+  const checkboxFondAwesome = document.createElement("i");
+  checkboxFondAwesome.className = "far fa-square";
+
+  checkbox.addEventListener("click", () => {
+    if(checkbox.checked) {
+      todoContentElement.style.textDecoration = "line-through";
+    } else {
+      todoContentElement.style.textDecoration = "none";
+    }
+  });
+
+  // Edit icon 
+  const todoEditButtonElement = document.createElement("button");
+  const todoEditIconElement = document.createElement("i");
+  todoEditIconElement.className = "fas fa-edit";
+   todoEditButtonElement.addEventListener("click", () => {
+     if (todoEditButtonElement.innerHTML == '<i class="fas fa-edit"></i>') {
+       todoEditButtonElement.innerText = "Save";
+       todoInputElement.removeAttribute("readonly"); 
+       todoInputElement.focus();
+     } else {
+       todoEditButtonElement.innerHTML = '<i class="fas fa-edit"></i>';
+       todoInputElement.setAttribute("readonly", "");
+    }
+   });
 
 
+   //Delete icon
+  const todoDeleteButtonElement = document.createElement("button");
+  const todoDeleteIconElement = document.createElement("i");
+  todoDeleteIconElement.className = "fas fa-trash";
+  todoDeleteButtonElement.addEventListener("click", () => {
+    listaParinteElement.removeChild(todoElement);
+  });
+
+  // actions
+  const todoActionsElement = document.createElement("div");
+  todoActionsElement.classList.add("actions");
+  checkbox.appendChild(checkboxFondAwesome);
+  todoEditButtonElement.appendChild(todoEditIconElement);
+  todoDeleteButtonElement.appendChild(todoDeleteIconElement);
+  todoActionsElement.appendChild(checkbox);
+  todoActionsElement.appendChild(todoEditButtonElement);
+  todoActionsElement.appendChild(todoDeleteButtonElement);
+
+  todoElement.appendChild(todoContentElement);
+  todoElement.appendChild(todoActionsElement);
+
+  listaParinteElement.appendChild(todoElement);
+}
+
+// Event de adaugare todo
+addTodoElement.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  showNotification(inputElement.value);
+
+  if (!inputElement.value) return;
+
+  createTodo(inputElement.value);
+
+  const link = document.createElement("link");
+link.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css";
+link.rel = "stylesheet";
+document.head.appendChild(link);
+
+  inputElement.value = "";
+});
